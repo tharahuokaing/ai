f509000bf913853174989e283a49da30aa482048
THARA HUOKAING <tharahuokaing@gmail.com>
2026-02-23 16:27:36 +0700

    Update and rename ghost_core.js to utils.js

---
+++ utils.js
@@ -0,0 +1,56 @@
+// Utility functions
+function speak(text) {
+    const utter = new SpeechSynthesisUtterance(text);
+    utter.lang = 'km-KH'; 
+    window.speechSynthesis.speak(utter);
+}
+
+function calculateSimilarity(s1, s2) {
+    let longer = s1;
+    let shorter = s2;
+    if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+    }
+    if (longer.length === 0) return 1.0;
+    return (longer.length - editDistance(longer, shorter)) / parseFloat(longer.length);
+}
+
+function editDistance(s1, s2) {
+    let costs = [];
+    for (let i = 0; i <= s1.length; i++) {
+        let lastValue = i;
+        for (let j = 0; j <= s2.length; j++) {
+            if (i === 0) costs[j] = j;
+            else if (j > 0) {
+                let newValue = costs[j - 1];
+                if (s1.charAt(i - 1) !== s2.charAt(j - 1))
+                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                costs[j - 1] = lastValue; 
+                lastValue = newValue;
+            }
+        }
+        if (i > 0) costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+}
+
+function saveState() {
+    const state = {
+        brain,
+        learningMode,
+        isEmperorMode,
+        singularityProgress: singularityRoadmap.progress
+    };
+    localStorage.setItem('ghost_state', JSON.stringify(state));
+}
+
+function loadState() {
+    const state = JSON.parse(localStorage.getItem('ghost_state') || '{}');
+    brain = state.brain || {};
+    learningMode = state.learningMode || null;
+    isEmperorMode = state.isEmperorMode || false;
+    if (state.singularityProgress) {
+        singularityRoadmap.progress = state.singularityProgress;
+    }
+}
